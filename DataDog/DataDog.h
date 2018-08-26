d57ef26047fbaeaf990e97558d03247789126250
Britton Katnich <katnich@shaw.ca>
2018-08-25 22:54:30 -0700

    Initial Commit
    Initial commit

---
+++ DataDog/DataDog.h
@@ -0,0 +1,19 @@
+//
+//  DataDog.h
+//  DataDog
+//
+//  Created by Britton Katnich on 2018-08-25.
+//  Copyright © 2018 DataDog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DataDog.
+FOUNDATION_EXPORT double DataDogVersionNumber;
+
+//! Project version string for DataDog.
+FOUNDATION_EXPORT const unsigned char DataDogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataDog/PublicHeader.h>
+
+
